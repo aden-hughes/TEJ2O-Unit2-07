2eac3a69939381da9fec9ec12ec4908506195c88
aden-hughes <aden.hughes@stu.ocsb.ca>
2026-03-03 13:24:33 -0500

    Made a cookie clicker game

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Aden Hughes
+ * Created on: Mar 3
+ * This program Simulates a cookie game
 */
 
-basic.showString('Hello, World!')
+// setting up for game
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let cookies = 0
+
+// starting the game
+input.onButtonPressed(Button.A, function () {
+    cookies += 1
+    basic.showNumber(cookies)
+})
+
+// resetting game
+input.onButtonPressed(Button.B, function () {
+    cookies = 0
+    basic.clearScreen()
+})
